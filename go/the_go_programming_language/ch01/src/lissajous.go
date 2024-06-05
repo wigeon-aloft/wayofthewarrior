cd20f38f4ef5a51e738408b8999f60fff4d28b63
Wigeon <willem@thevanhoofs.com>
2024-06-05 17:20:12 +0100

    Updated lissajous.go to use green on black colour scheme

--- go/the_go_programming_language/ch01/src/lissajous.go
+++ go/the_go_programming_language/ch01/src/lissajous.go
@@ -11,7 +11,7 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
 const (
 	whiteIndex = 0 // first color in palette
