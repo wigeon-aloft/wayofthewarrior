52d0a96c4078476d013466d23bed07bdad18f07e
Wigeon <willem@thevanhoofs.com>
2024-06-07 16:37:36 +0100

    Added source and build for fetchall.go

---
+++ go/the_go_programming_language/ch01/src/fetchall.go
@@ -0,0 +1,42 @@
+// Fetchall fetches URLs in parallel and reports their
+// times and sizes using channels and go routines
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(io.Discard, resp.Body) // Dump the response body but keep the number of bytes
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
